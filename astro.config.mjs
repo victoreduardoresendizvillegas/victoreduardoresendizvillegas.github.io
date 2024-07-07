cb71edb6f45195c0ae2a32a04f581dade058566c
Ariel Cerda <metabolic.ds@gmail.com>
2024-07-07 15:55:23 +1200

    Add FontPicker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import fontPicker from "astro-font-picker";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), fontPicker()]
 });
