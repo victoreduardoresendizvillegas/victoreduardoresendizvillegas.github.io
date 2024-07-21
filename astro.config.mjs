112e3ca32949aa0cc3b39fa0f26e0959e3464240
Ariel Cerda <metabolic.ds@gmail.com>
2024-07-21 22:28:13 +1200

    remove fontPicker, layout adjustments, fix education links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import fontPicker from "astro-font-picker";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), fontPicker()]
+  integrations: [tailwind()],
 });
